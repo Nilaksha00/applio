17898fe8d7597cd055240c1014c34fb925bfe87e
Nilaksha Perera <nilakshasperera@gmail.com>
2026-03-13 17:11:05 +0000

    Home page: Basic card UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
+import Navbar from "~/components/Navbar"
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { useState } from "react";
+import {resumes} from "~/constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,6 +12,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main >
-    </main>;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+         <Navbar />
+          <section className="main-section">
+            <div className="page-heading">
+              <h1>Track Your Applications & Resume Ratings</h1>
+              <h2>Review your submissions and check AI-powered feedback</h2>
+            </div>
+          </section>
+
+       { resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>)}
+
+
+        </main>;
 }
